ad4c33395d2803cb455889c3217b3b080d0dcfff
Timurqr <berezhnoi.work@gmail.com>
2022-01-08 12:31:39 +0200

    Add project files.

---
+++ IntroductionAppCalculateUserAge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroductionAppCalculateUserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
